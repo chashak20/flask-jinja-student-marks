e33c1bb1c22c7fa111b7f35b68b3eaab68f4d954
chashak20 <chashak03@gmail.com>
2026-03-16 11:31:43 +0530

    Flask Jinga template project displaying student marks

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    marks={
+        "John":45,
+        "Vishnu":60,
+        "Rahul":70,
+        "Alexa":50,
+        "Krish":55,
+        "Roshan":70,
+        "Neha":65,
+        "Mayur":72,
+        "Deep":85
+    }
+    return render_template("index.html", marks=marks)
+app.run(debug=True)
